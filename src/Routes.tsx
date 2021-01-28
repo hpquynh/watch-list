33acf994a32a19805a10463fe4358a8b56a944b9
Quynh HP <qho@openwt.com>
2021-01-28 17:14:37 +0700

    refactor route

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,13 +1,11 @@
 import React, {ReactElement} from 'react';
 import {Route, Switch} from 'react-router-dom';
-import {routeProps} from '~/src/configs/routeProps';
+import {WatchList} from './pages';
 
 export const Routes: React.FC = (): ReactElement => {
     return (
         <Switch>
-            {routeProps.map((route, index) => (
-                <Route key={index} {...route} />
-            ))}
+            <Route path="/" component={WatchList} exact/>
             <Route path="*">
                 404 not found
             </Route>
--- src/configs/routeProps.ts
+++
@@ -1,7 +0,0 @@
-import {RouteProps} from 'react-router';
-import {WatchList, WatchDetail} from '~/src/pages';
-
-export const routeProps: RouteProps[] = [
-    {path: '/', component: WatchList, exact: true},
-    {path: '/detail/:id', children: WatchDetail}
-];
